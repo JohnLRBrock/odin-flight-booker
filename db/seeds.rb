a25aded989fe08839b2bdf0f7ab126d6096e363a
khanthulhu <johnlrbrock@gmail.com>
2017-03-19 22:09:02 -0400

    added more flights in seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,12 @@ airports = Airport.create([{ code: 'SFO' }, { code: 'JFK' },
                            { code: 'TPA' }, { code: 'ATL '}
                           ])
 
-flights = Flight.create([{ start_airport_id: 1, end_airport_id: 2,
-                           date: Date.today, start_time: Time.now, duration_in_seconds: 14400},
-                         { start_airport_id: 1, end_airport_id: 3,
-                           date: Date.today, start_time: Time.now, duration_in_seconds: 14400},
-                         { start_airport_id: 2, end_airport_id: 1,
-                           date: Date.today, start_time: Time.now, duration_in_seconds: 14400},
-                         { start_airport_id: 2, end_airport_id: 3,
-                           date: Date.today, start_time: Time.now, duration_in_seconds: 14400},
-                         { start_airport_id: 2, end_airport_id: 4,
-                           date: Date.today, start_time: Time.now, duration_in_seconds: 14400},
-                         { start_airport_id: 3, end_airport_id: 4,
-                           date: Date.today, start_time: Time.now, duration_in_seconds: 14400
-                         }])
+1000.times do
+  flight = Flight.new()
+  flight.start_airport_id    = rand(1..4)
+  flight.end_airport_id      = rand(1..4)
+  flight.date                = Date.today + 20.years + rand(-7..7).days
+  flight.start_time          = Time.now + rand(-3600..3600).seconds
+  flight.duration_in_seconds = 14400 + rand(-3600..3600)
+  flight.save
+end
